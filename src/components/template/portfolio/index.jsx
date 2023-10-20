d210718b2250107b56a91bf1fe1b4fa386f6d35f
hungtruong97 <hungtruong97@gmail.com>
2023-10-21 06:43:20 +0700

    header partial

--- src/components/template/portfolio/index.jsx
+++ src/components/template/portfolio/index.jsx
@@ -1,13 +1,36 @@
-import Career from "../../organisms/Career";
-import HeaderSec from "../../organisms/HeaderSec";
-import Hero from "../../organisms/Hero";
+import styles from "../portfolio/styles.module.scss";
 
 const Portfolio = () => {
   return (
     <>
-      <HeaderSec></HeaderSec>
-      <Hero></Hero>
-      <Career></Career>
+      <section id={styles["header"]}>
+        <div className={styles["container"]}>
+          <div className={styles["content"]}>
+            <a href="#">
+              <span className={styles["header_button"]}>Home</span>
+            </a>
+            <a href="#">
+              <span className={styles["header_button"]}>About me</span>
+            </a>
+            <a href="#">
+              <span className={styles["header_button"]}>
+                Skills & Abilities
+              </span>
+            </a>
+            <a href="#">
+              <span className={styles["header_button"]}>Career Highlights</span>
+            </a>
+            <a href="#">
+              <span className={styles["header_button"]}>Contact Me</span>
+            </a>
+          </div>
+        </div>
+      </section>
+      <section className="hero"></section>
+      <section className="about"></section>
+      <section className="skills"></section>
+      <section className="career"></section>
+      <section className="contact"></section>
     </>
   );
 };
