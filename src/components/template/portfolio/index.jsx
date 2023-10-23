57814e3e5d8a6d0896d8b76d8559cd5c369daacf
hungtruong97 <hungtruong97@gmail.com>
2023-10-23 14:46:56 +0700

    header draft 1

--- src/components/template/portfolio/index.jsx
+++ src/components/template/portfolio/index.jsx
@@ -1,32 +1,48 @@
 import styles from "../portfolio/styles.module.scss";
+import { useState } from "react";
 
 const Portfolio = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <section id={styles["header"]}>
         <div className={styles["container"]}>
           <div className={styles["content"]}>
-            <a href="#">
-              <span className={styles["header_button"]}>Home</span>
-            </a>
-            <a href="#">
-              <span className={styles["header_button"]}>About me</span>
-            </a>
-            <a href="#">
-              <span className={styles["header_button"]}>
-                Skills & Abilities
-              </span>
-            </a>
-            <a href="#">
-              <span className={styles["header_button"]}>Career Highlights</span>
-            </a>
-            <a href="#">
-              <span className={styles["header_button"]}>Contact Me</span>
-            </a>
+            <div className={styles["hamburger"]} onClick={toggleMenu}>
+              <i className="fa-solid fa-bars"></i>
+            </div>
+            <div className={`${styles.menu} ${menuOpen ? styles.open : ""}`}>
+              <a href="#">
+                <span className={styles["header_button"]}>Home</span>
+              </a>
+              <a href="#">
+                <span className={styles["header_button"]}>About me</span>
+              </a>
+              <a href="#">
+                <span className={styles["header_button"]}>
+                  Skills & Abilities
+                </span>
+              </a>
+              <a href="#">
+                <span className={styles["header_button"]}>
+                  Career Highlights
+                </span>
+              </a>
+              <a href="#">
+                <span className={styles["header_button"]}>Contact Me</span>
+              </a>
+            </div>
           </div>
         </div>
       </section>
-      <section className="hero"></section>
+      <section className="hero">
+        <div></div>
+      </section>
       <section className="about"></section>
       <section className="skills"></section>
       <section className="career"></section>
