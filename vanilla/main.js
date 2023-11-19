32a86921457aeb29cfe1965d53ab441eff911022
hungtruong97 <hungtruong97@gmail.com>
2023-11-19 20:33:23 +0700

    header

--- vanilla/main.js
+++ vanilla/main.js
@@ -1,24 +1,18 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const app = document.querySelector("#app");
 
-setupCounter(document.querySelector('#counter'))
+const toggleMenu = () => {
+  const menuOverlay = document.querySelector(".menu_overlay");
+  console.log("clicked");
+  menuOverlay.classList.toggle("open");
+};
+
+const closeMenuButton = document.querySelector(".closeMenuButton");
+closeMenuButton.addEventListener("click", toggleMenu);
+
+const hamburgerMenuButton = document.querySelector(".hamburger");
+hamburgerMenuButton.addEventListener("click", toggleMenu);
