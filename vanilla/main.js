9f35b03519d4b68fa88bdbdf72ec292109f348a9
hungtruong97 <hungtruong97@gmail.com>
2023-11-26 13:46:24 +0700

    full page

--- vanilla/main.js
+++ vanilla/main.js
@@ -15,3 +15,19 @@ closeMenuButton.addEventListener("click", toggleMenu);
 
 const hamburgerMenuButton = document.querySelector(".hamburger");
 hamburgerMenuButton.addEventListener("click", toggleMenu);
+
+// Theme toggler
+const themeToggleButton = document.getElementById("themeToggler");
+const currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : localStorage.setItem("theme", "light");
+document.documentElement.setAttribute("data-theme", currentTheme);
+
+themeToggleButton.addEventListener("click", () => {
+  let theme = "light"; // default to light
+  if (document.documentElement.getAttribute("data-theme") === "light") {
+    theme = "dark";
+  }
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme); // store theme preference in local storage
+});
